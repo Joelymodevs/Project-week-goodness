e4c66fa0ec5cf12dd6d21ae8c9b01bdf559845a2
Joel Morton <joelmortondev@gmail.com>
2023-05-30 11:01:20 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express();
 const endpoints = require("./endpoints.json");
 const { fetchTopics, fetchEndpoints, fetchAllArticles, fetchArticleById } = require("./controllers");
 const { handle500, psqlErr, handleCustom } = require("./errorhandles");
-
+const cors = require('cors')
+app.use(cors())
 app.get("/api", fetchEndpoints)
 app.get("/api/topics", fetchTopics);
 app.get('/api/articles', fetchAllArticles)
